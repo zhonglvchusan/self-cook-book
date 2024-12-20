3e39feded12550c0b1dbba180348954c38c3e234
wangzh <3364462922@qq.com>
2024-12-20 14:43:10 +0800

    feat: 搭建apollo，暂时注释nacos注册

--- src/main/java/wang/zehui/selfcookbookjava/SelfCookbookJavaApplication.java
+++ src/main/java/wang/zehui/selfcookbookjava/SelfCookbookJavaApplication.java
@@ -1,9 +1,11 @@
 package wang.zehui.selfcookbookjava;
 
+import com.ctrip.framework.apollo.spring.annotation.EnableApolloConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableApolloConfig
 public class SelfCookbookJavaApplication {
 
     public static void main(String[] args) {
