5e6dde3a30c7082b684210a7ede162f6f7d45ea3
wangzh <3364462922@qq.com>
2026-04-02 15:02:17 +0800

    feat: 增加mvc配置信息

---
+++ src/main/java/wang/zehui/self/cook/book/common/config/MvcConfig.java
@@ -0,0 +1,32 @@
+package wang.zehui.self.cook.book.common.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import wang.zehui.self.cook.book.common.interceptor.AuthInterceptor;
+
+/**
+ * @Author wangzehui
+ * @Date 2026/4/2 15:00
+ */
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+
+    @Autowired
+    private AuthInterceptor authInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(authInterceptor)
+                .excludePathPatterns(OpenApiConfig.OPEN_API_WHITELIST)
+                .addPathPatterns("/**");
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("doc.html").addResourceLocations("classpath:/META-INF/resources/");
+        registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
+}
