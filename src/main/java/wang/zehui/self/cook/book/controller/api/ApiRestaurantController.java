9c5ab4b38c45a8d8a20813404c08535a8b42164c
wangzh <3364462922@qq.com>
2026-06-12 12:01:07 +0800

    feat: api接口增加餐厅列表

--- src/main/java/wang/zehui/self/cook/book/controller/api/ApiRestaurantController.java
+++ src/main/java/wang/zehui/self/cook/book/controller/api/ApiRestaurantController.java
@@ -4,8 +4,11 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+import wang.zehui.self.cook.book.common.domain.PageResult;
 import wang.zehui.self.cook.book.domain.request.RestaurantRequest;
+import wang.zehui.self.cook.book.domain.request.RestaurantSearchRequest;
 import wang.zehui.self.cook.book.domain.response.ResponseDTO;
+import wang.zehui.self.cook.book.domain.response.RestaurantListResponse;
 import wang.zehui.self.cook.book.domain.response.RestaurantResponse;
 import wang.zehui.self.cook.book.service.IRestaurantService;
 
@@ -25,13 +28,19 @@ public class ApiRestaurantController {
 
     @GetMapping
     @Operation(summary = "获取餐厅信息 @author wangzh")
-    public ResponseDTO<RestaurantResponse> getRestaurantInfo() {
-        return ResponseDTO.success(restaurantService.getRestaurantInfo(null));
+    public ResponseDTO<RestaurantResponse> getRestaurantInfo(String restaurantId) {
+        return ResponseDTO.success(restaurantService.getRestaurantInfo(restaurantId));
     }
 
     @PostMapping
     @Operation(summary = "添加/修改餐厅信息 @author wangzh")
     public ResponseDTO<Boolean> addOrUpdateRestaurant(@RequestBody @Validated RestaurantRequest request) {
         return ResponseDTO.success(restaurantService.addOrUpdateRestaurant(request));
+    }
+
+    @GetMapping("list")
+    @Operation(summary = "获取餐厅分页列表 @author wangzh")
+    public ResponseDTO<PageResult<RestaurantListResponse>> getRestaurantPageList(RestaurantSearchRequest request) {
+        return ResponseDTO.success(restaurantService.getRestaurantPageList(request));
     }
 }
